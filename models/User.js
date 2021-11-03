f7c0561def88414bcdba3f3fac84c4b4d0991430
KANGSOONYONG <pliuhb@naver.com>
2021-11-03 17:32:13 +0900

    로그인 기능, auth 기능, 로그아웃 기능 구현완료

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 
 // salt를 이용해서 비밀번호 암호화, saltRounds는 몇자리 수인지 설정
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -53,6 +54,44 @@ userSchema.pre('save', function( next ){
         next()
     }
 })
+
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+
+    // plainPassword => 암호화 X 비밀번호 랑 암호화된 비밀번호랑 비교해야 함
+    // 어떻게? -> plainPassword를 암호화를 한 다음 비교를 해야함 (암호화된 것을 복구할 수 없기 때믄)
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+            cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+
+    var user = this;
+
+    // jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에 
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
